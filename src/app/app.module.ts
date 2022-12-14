230c3ae8081e4da56b1def7e54b5c5716339ffe1
MAllena <MAllena@rosariobus.com.ar>
2022-12-14 14:48:07 -0300

    shared_assets con logos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
