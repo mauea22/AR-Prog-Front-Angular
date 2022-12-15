699717669b850f3d5be18258f74a985a8cb9adb4
MAllena <MAllena@rosariobus.com.ar>
2022-12-15 12:18:13 -0300

    social-media.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { LogoApComponent } from './shared/logo-ap/logo-ap.component';
+import { SocialMediaComponent } from './shared/social-media/social-media.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoApComponent
+    LogoApComponent,
+    SocialMediaComponent
   ],
   imports: [
     BrowserModule,
